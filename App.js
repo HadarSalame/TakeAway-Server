00807d1d717aa6df0f1b6b9e1781b1864976f592
HadarSalame <hadarSalame99@gmail.com>
2022-05-11 11:19:40 +0300

    installations

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
 const express= require ('express');
 const App =express();
+const  env  = require('dotenv');
+env.config();
+const path = require("path")
+const bodyParser = require("body-parser")
+App.use(bodyParser.json())
+const mongoose=require('mongoose')
+mongoose.connect(process.env.MONGO_CONNECTION,{
+    
+})
+.then(()=>{
+console.log("connect to mongo")
+}).catch(err=>{ "myerr" + err })
+
 
 App.listen(()=>{
     console.log('listening on 3030')
